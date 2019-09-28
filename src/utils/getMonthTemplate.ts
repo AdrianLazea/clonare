1886c289142f17788880120cc637701318518612
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 19:53:49 +0300

    Added methods to return first day and number of days

---
+++ src/utils/getMonthTemplate.ts
@@ -0,0 +1,31 @@
+// Function that returns an html template for a month
+// firstDay - eg: Mon = 1, Sunday = 7
+// numberOfDays - From 28 to 31
+export default function(
+  firstDay: number,
+  numberOfDays: number,
+  currentDay: number
+): String {
+  let monthStarted = false;
+  let daysCount = 1;
+  let monthTemplate = "";
+
+  while (daysCount <= numberOfDays) {
+    for (let i = 0; i < 7; i++) {
+      if (i + 1 === firstDay && !monthStarted) monthStarted = true;
+      if ((i + 1 !== firstDay && !monthStarted) || daysCount > numberOfDays) {
+        // if the month hasn't started yet or the daysCount is bigger than the number of days in the month
+        // just add an empty span
+        monthTemplate += `<span data-day="null" class='day empty'></span>`;
+      } else {
+        // else, add the day number in the span
+        monthTemplate += `<span data-day="${daysCount}" class='day ${
+          currentDay === daysCount ? "current" : ""
+        }'>${daysCount++}</span>`;
+      }
+    }
+    monthTemplate += `<br/>`;
+  }
+
+  return monthTemplate;
+}
