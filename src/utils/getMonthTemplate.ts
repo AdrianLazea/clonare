c06fce961bd34afb95809950731cf4ff2c4c1dcd
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 21:11:29 +0300

    Reverting back to old function. Mistake found in function call, was putting year instead of firstDay

--- src/utils/getMonthTemplate.ts
+++ src/utils/getMonthTemplate.ts
@@ -10,14 +10,14 @@ export default function(
   let daysCount = 1;
   let monthTemplate = "";
 
-  for (let week = 1; week <= 5; week++) {
-    for (let i = 1; i <= 7; i++) {
-      if (i === firstDay && !monthStarted) monthStarted = true;
-      if (i !== firstDay && !monthStarted) {
+  while (daysCount <= numberOfDays) {
+    for (let i = 0; i < 7; i++) {
+      if (i + 1 === firstDay && !monthStarted) monthStarted = true;
+      if ((i + 1 !== firstDay && !monthStarted) || daysCount > numberOfDays) {
         // if the month hasn't started yet or the daysCount is bigger than the number of days in the month
         // just add an empty span
         monthTemplate += "<span data-day='null' class='day empty'></span>";
-      } else if (daysCount <= numberOfDays) {
+      } else {
         // else, add the day number in the span
         monthTemplate += `<span data-day="${daysCount}" class='day ${
           currentDay === daysCount ? "current" : ""
